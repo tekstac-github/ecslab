86f4c8be0276ac8c4588f792e8a1b8bb89ec3386
tekstac-github <88327767+tekstac-github@users.noreply.github.com>
2023-11-09 17:40:03 +0530

    Add files via upload

---
+++ ecsparam.sh
@@ -0,0 +1,21 @@
+echo "###### ECR REPOSITORY_URI #######"
+export REPOSITORY_URI=$(aws ecr describe-repositories --query 'repositories[].[repositoryUri]' --output text)
+echo -e  "$REPOSITORY_URI \n"
+
+echo "###### Subnets ######"
+subnets=$(aws ec2 describe-subnets | jq -r .Subnets[].SubnetId)
+echo -e "$subnets \n"
+
+echo "###### ecstaskdefinitionrole #######"
+ecstaskdefinitionrole=$(aws iam list-roles | jq -r .Roles[].Arn | grep ecstaskdefinitionrole)
+echo  -e "$ecstaskdefinitionrole \n"
+
+
+echo "####### target-group #######"
+targetArn=$(aws elbv2 describe-target-groups | jq -r .TargetGroups[].TargetGroupArn)
+echo -e "$targetArn \n"
+
+
+echo "####### security group #######"
+securitygroup=$(aws ec2 describe-security-groups --filters Name=tag:Name,Values=Web-SG | jq -r .SecurityGroups[].GroupId)
+echo -e "$securitygroup \n"
