c4ac2444d806fe899523657064dca5b8abc23108
tekstac-github <88327767+tekstac-github@users.noreply.github.com>
2023-11-10 12:16:49 +0530

    Update ecsparam.sh

--- ecsparam.sh
+++ ecsparam.sh
@@ -19,3 +19,7 @@ echo -e "$targetArn \n"
 echo "####### security group #######"
 securitygroup=$(aws ec2 describe-security-groups --filters Name=tag:Name,Values=Web-SG | jq -r .SecurityGroups[].GroupId)
 echo -e "$securitygroup \n"
+
+echo "####### DNSName of the Load Balancer ######"
+LoadbalancerURL=$(aws elbv2 describe-load-balancers | jq -r .LoadBalancers[].DNSName)
+echo -e "$LoadbalancerURL \n"
